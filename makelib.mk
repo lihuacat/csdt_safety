550fff69f73dbc77117f79447a73c605854c6059
lihuacat <32027153+lihuacat@users.noreply.github.com>
2018-11-18 10:14:55 +0800

    Add files via upload

---
+++ makelib.mk
@@ -0,0 +1,32 @@
+#CROSS_COMPILE=mips-linux-uclibc-gnu-
+CC=$(CROSS_COMPILE)gcc
+CPP=$(CROSS_COMPILE)g++
+AS=$(CROSS_COMPILE)as
+LD=$(CROSS_COMPILE)ld
+STRIP=$(CROSS_COMPILE)strip
+
+ROOTDIR := .
+SRCDIR := $(ROOTDIR)/src
+INCLUDE := -I $(ROOTDIR)/src
+# INCLUDE += -I $(SRCDIR)mempool
+SRCS := $(SRCDIR)/cstd_safety.c
+
+OBJS := $(SRCDIR)/cstd_safety.o
+
+
+LIBS := -lm
+
+# CFLAGS := $(INCLUDE) $(LIBDIR) -Wall -O2 -ffunction-sections -fdata-sections -Wl,-Map=object.map,--cref,--gc-section -fvisibility=hidden
+CFLAGS := $(INCLUDE) $(LIBDIR) -Wall -O2 -ffunction-sections -fdata-sections -Wl,-Map=object.map,--cref,--gc-section -fPIC
+
+TARGET := libcstd_safety.so 
+
+all:${OBJS}
+	$(CC) $(CFLAGS) -shared -fPIC -o $(TARGET) ${OBJS} $(LIBS) 
+	$(STRIP) $(TARGET) 
+	@cp $(TARGET) ./lib/
+
+clean:
+	@find . -name "*.o"  | xargs rm -f
+	@rm -rf $(TARGETS)
+
